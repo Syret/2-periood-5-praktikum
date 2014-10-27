5196e6269b0fc00e04b157504600af683d07e698
Syret <kart.syret@khk.ee>
2014-10-27 07:45:36 +0200

    Commit

---
+++ validation.php
@@ -0,0 +1,14 @@
+<?php	
+	$isSubmitted = isset($_POST["submit"]);
+	if (isset($_POST["submit"])) {
+		$username = $_POST["username"];
+	}
+
+	if (!isset($username) || $username == "") {
+		$usernameMessage= '<div class= "form_message"> Palun sisesta kasutajanimi</div>';
+		
+	} else {
+		$usernameMessage='<div class= "form_message"> Kasutajanimi sobis </div>';
+
+	}
+?>
